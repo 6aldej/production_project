6c4e0afd9ede040204e10632820e86c4172c29db
Кокорев Николай Игоревич <>
2023-02-20 22:02:54 +0300

    themeSwitcher

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,7 @@
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppLink } from "shared/ui/AppLink";
 import { AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
 
 import cls from './Navbar.module.scss'
 
@@ -11,6 +12,7 @@ interface NavbarProps {
 export const Navbar = ({className}: NavbarProps) => {
   return (
     <div className={classNames(cls.navbar, {}, [className])}>
+      <ThemeSwitcher />
       <div className={cls.links}>
         <AppLink 
           theme={AppLinkTheme.SECONDARY} 
