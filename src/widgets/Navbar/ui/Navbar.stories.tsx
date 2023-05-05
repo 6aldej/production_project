dd0b63c687bf975386feaf3cbc5c80b47b22b91e
Кокорев Николай Игоревич <>
2023-05-05 15:44:44 +0300

    test ui report

--- src/widgets/Navbar/ui/Navbar.stories.tsx
+++ src/widgets/Navbar/ui/Navbar.stories.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
 import { ThemeDecorator } from 'shared/configs/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
-import { NotFoundPage } from './NotFoundPage';
+import { Navbar } from './Navbar';
 
 export default {
-  title: 'pages/NotFoundPage',
-  component: NotFoundPage,
+  title: 'widget/Navbar',
+  component: Navbar,
   argTypes: {
     backgroundColor: { control: 'color' },
   },
-} as ComponentMeta<typeof NotFoundPage>;
+} as ComponentMeta<typeof Navbar>;
 
-const Template: ComponentStory<typeof NotFoundPage> = () => <NotFoundPage />;
+const Template: ComponentStory<typeof Navbar> = (args) => <Navbar {...args} />;
 
-export const Normal = Template.bind({});
-Normal.args = {};
+export const Light = Template.bind({});
+Light.args = {};
 
 export const Dark = Template.bind({});
 Dark.args = {};
