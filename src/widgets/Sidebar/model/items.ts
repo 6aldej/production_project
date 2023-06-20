f138261d96749ba8704351a922875c1107ec3782
Кокорев Николай Игоревич <>
2023-06-20 14:26:49 +0300

    add ArticlesPage, ArticleDetailsPage

--- src/widgets/Sidebar/model/items.ts
+++ src/widgets/Sidebar/model/items.ts
@@ -3,6 +3,7 @@ import { RoutePath } from 'shared/configs/routeConfig/routeConfig';
 import AboutIcon from 'shared/assets/icons/about.svg';
 import HomeIcon from 'shared/assets/icons/home.svg';
 import ProfileIcon from 'shared/assets/icons/profile.svg';
+import ArticleIcon from 'shared/assets/icons/article.svg';
 
 export interface SidebarItemType {
   path: string,
@@ -21,6 +22,12 @@ export const SidebarItemsList: SidebarItemType[] = [
     path: RoutePath.about,
     text: 'О сайте',
     icon: AboutIcon,
+  },
+  {
+    path: RoutePath.articles,
+    text: 'Статьи',
+    icon: ArticleIcon,
+    authOnly: true,
   },
   {
     path: RoutePath.profile,
